0e86eebb3bda890e3c1325bf9128dd8008c860cd
Brendan Coates <brnco@peanut.local>
2019-06-02 16:28:06 -0700

    updates

--- data_mover.py
+++ data_mover.py
@@ -17,7 +17,6 @@ import contextlib
 import ghandler as gh
 import gspread
 import subprocess
-import dropbox
 
 class cd:
     """Context manager for changing the current working directory"""
@@ -149,7 +148,8 @@ def moveDropboxToTraffic(args):
             #output = subprocess.check_output('dropbox filestatus "' + fullpath + '"' )
             output = "/root/Dropbox/MF archival audio/20170225_PalmDesertAct2_T585.mp3: up to date"
             outList = output.split(":")
-            if outList[1].lstrip() = "up to date":
+            status = outList[1].lstrip()
+            if status = "up to date":
                 #rsync
                 print("ready to move file " + f)
             else:

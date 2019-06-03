80ec2d44fc0cf4d1a8fba52a37b658a44acb304e
Brendan Coates <brnco@peanut.local>
2019-06-02 17:20:21 -0700

    updates 17:20

--- data_mover.py
+++ data_mover.py
@@ -156,6 +156,7 @@ def moveDropboxToTraffic(args):
                 fullpath = os.path.join(dirs,f)
                 with cd(args.Dropbox):
                     output = subprocess.check_output('dropbox filestatus "' + f + '"', shell=True)
+                    output = output.communicate()
                 #output = "/root/Dropbox/MF archival audio/20170225_PalmDesertAct2_T585.mp3: up to date"
                 outList = output.split(":")
                 status = outList[1].lstrip()
